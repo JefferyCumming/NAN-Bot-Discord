533eca7a1d760b89747a94a1058b517398d7d995
Jeffery Cumming <16328521+JefferyCumming@users.noreply.github.com>
2019-12-27 22:41:13 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,29 @@
 import random
+import discord
+from discord.ext import commands
+import urllib.request
+from urllib.request import urlopen
+import json
 
+# Word bank for random text function
 word_bank = ("Lemon", "Rag", "Goat", "Funk", "Crunch", "Suculant", "Tastey", "Juice", "Grilled", "Mario", "Hot", "Stink")
-print(word_bank[random.randrange(0,16)] + ' ' + word_bank[random.randrange(0,16)] + ' ' + word_bank[random.randrange(0,16)] + ' ' + word_bank[random.randrange(0,16)])
+
+#Set Discord API bot token here
+TOKEN = ''
+
+description = "Sandwich text using NAN"
+bot = commands.Bot(command_prefix='!', description=description)
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def start():
+    await bot.say(print(word_bank[random.randrange(0,16)] + ' ' + word_bank[random.randrange(0,16)] + ' ' + word_bank[random.randrange(0,16)] + ' ' + word_bank[random.randrange(0,16)]))
+
+
