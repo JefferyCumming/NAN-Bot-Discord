dc81ec43c64f4249ce183f610af59f475a792fa6
Jeffery Cumming <16328521+JefferyCumming@users.noreply.github.com>
2019-12-27 22:26:17 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,4 @@
+import random
+
+word_bank = ("Lemon", "Rag", "Goat", "Funk", "Crunch", "Suculant", "Tastey", "Juice", "Grilled", "Mario", "Hot", "Stink")
+print(word_bank[random.randrange(0,16)] + ' ' + word_bank[random.randrange(0,16)] + ' ' + word_bank[random.randrange(0,16)] + ' ' + word_bank[random.randrange(0,16)])
